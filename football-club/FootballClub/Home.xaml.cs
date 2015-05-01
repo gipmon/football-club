9342812470bdfbd724535455bb79780236ede664
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-05-01 14:34:37 +0100

    ++

--- football-club/FootballClub/Home.xaml.cs
+++ football-club/FootballClub/Home.xaml.cs
@@ -10,19 +10,26 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace FootballClub
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class Home : MetroWindow
     {
-        public MainWindow()
+        public Home()
         {
             InitializeComponent();
+        }
+
+        private void Button_ClubMember(object sender, RoutedEventArgs e)
+        {
+            ClubMember clubMember = new ClubMember();
+            this.NavigationService.Navigate(clubMember);
         }
     }
 }
