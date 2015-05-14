a5d22487237530bdf021ed8645746aabbf73dab0
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-05-14 12:02:06 +0100

    members totally done

--- BD/SQL/store procedures/members.sql
+++ BD/SQL/store procedures/members.sql
@@ -77,3 +77,83 @@ AS
 		RAISERROR ('An error occurred when creating the member!', 14, 1)
 		ROLLBACK TRANSACTION;
 	END CATCH;
+
+	go
+
+-- DROP PROC football.sp_modifyMember
+
+CREATE PROCEDURE football.sp_modifyMember
+  @bi				INT, 
+  @name				VARCHAR(75),
+  @address			VARCHAR(75), 
+  @birth_date		DATE, 
+  @gender			VARCHAR(1), 
+  @nationality		VARCHAR(75),
+  @shares_value		MONEY,
+  @shares_in_day	BIT
+WITH ENCRYPTION
+AS 
+	IF @bi is null OR @name is null OR @address is null OR @birth_date is null OR 
+		@gender is null OR @nationality is null OR @shares_value is null OR
+		@shares_in_day is null
+	BEGIN
+		PRINT 'The bi, name, address, birth_date, nationality, shares value and shares in day can not be null!'
+		RETURN
+	END
+	
+	DECLARE @count int
+
+	-- check if the BI exists
+	SELECT @count = count(bi) FROM football.person WHERE bi = @bi;
+
+	IF @count = 0
+	BEGIN
+		RAISERROR ('The BI that you provided do not exists!', 14, 1)
+		RETURN
+	END
+
+	BEGIN TRANSACTION;
+
+	BEGIN TRY
+		UPDATE  football.person SET
+				name = @name, 
+				address = @address, 
+				birth_date = @birth_date,
+				gender = @gender,
+				nationality = @nationality
+		WHERE bi = @bi;
+
+		UPDATE football.members SET
+			   shares_value = @shares_value, 
+			   shares_in_day = @shares_in_day	   
+		WHERE bi = @bi;
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		RAISERROR ('An error occurred when updating the member!', 14, 1)
+		ROLLBACK TRANSACTION;
+	END CATCH;
+
+CREATE PROCEDURE football.sp_deleteMember
+  @bi				INT
+WITH ENCRYPTION
+AS 
+	IF @bi is null
+	BEGIN
+		PRINT 'The bi can not be null!'
+		RETURN
+	END
+	
+	BEGIN TRANSACTION;
+
+	BEGIN TRY
+		DELETE FROM football.members WHERE bi = @bi;
+		DELETE FROM football.person WHERE bi = @bi;
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		RAISERROR ('An error occurred when try delete the member!', 14, 1)
+		ROLLBACK TRANSACTION;
+	END CATCH;
+
--- football-club/FootballClub/ClubMember.xaml.cs
+++ football-club/FootballClub/ClubMember.xaml.cs
@@ -214,5 +214,156 @@ namespace FootballClub
             }
 
         }
+
+        private void Member_Update(object sender, RoutedEventArgs e)
+        {
+            using (con = new SqlConnection(ConString))
+            {
+                // --> Validations
+                int biInt;
+
+                // bi and federation id is number
+                if (!Int32.TryParse(member_bi.Text, out biInt))
+                {
+                    MessageBox.Show("The BI must be an Integer!");
+                    return;
+                }
+              
+                DateTime dt;
+                if (!DateTime.TryParse(member_birth_date.Text, out dt))
+                {
+                    MessageBox.Show("Please insert a valid date!");
+                    return;
+                }
+
+                if (member_name.Text.Length == 0)
+                {
+                    MessageBox.Show("The name can't be blank!");
+                    return;
+                }
+                if (member_address.Text.Length == 0)
+                {
+                    MessageBox.Show("The address can't be blank!");
+                    return;
+                }
+                if (member_nationality.Text.Length == 0)
+                {
+                    MessageBox.Show("The nationality can't be blank!");
+                    return;
+                }
+
+                string gender;
+                if (member_GenderFemale.IsChecked == true)
+                {
+                    gender = "F";
+                }
+                else if (member_GenderMale.IsChecked == true)
+                {
+                    gender = "M";
+                }
+                else
+                {
+                    MessageBox.Show("Please select the gender!");
+                    return;
+                }
+
+                string shares_in_day;
+                if (member_shares_in_day.IsChecked == true)
+                {
+                    shares_in_day = "True";
+                }
+                else if (member_shares_in_day.IsChecked == false)
+                {
+                    shares_in_day = "False";
+                }
+                else
+                {
+                    MessageBox.Show("Please select the shares situation!");
+                    return;
+                }
+
+                // UPDATE MEMBER
+                string CmdString = "football.sp_modifyMember";
+                SqlCommand cmd_member = new SqlCommand(CmdString, con);
+                cmd_member.CommandType = CommandType.StoredProcedure;
+                cmd_member.Parameters.AddWithValue("@bi", biInt);
+                cmd_member.Parameters.AddWithValue("@name", member_name.Text);
+                cmd_member.Parameters.AddWithValue("@address", member_address.Text);
+                cmd_member.Parameters.AddWithValue("@birth_date", dt);
+                cmd_member.Parameters.AddWithValue("@gender", gender);
+                cmd_member.Parameters.AddWithValue("@nationality", member_nationality.Text);
+                cmd_member.Parameters.AddWithValue("@shares_value", (double)member_shares_value.Value);
+                cmd_member.Parameters.AddWithValue("@shares_in_day", shares_in_day);
+                
+                try
+                {
+                    con.Open();
+                    cmd_member.ExecuteNonQuery();
+                    FillDataGridMembers(con);
+                    con.Close();
+                    MessageBox.Show("The member has been updated successfully!");
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+
+        }
+
+        private void Member_Delete(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Are you sure?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                using (con = new SqlConnection(ConString))
+                {
+                    // --> Validations
+                    int biInt;
+
+                    // bi is number
+                    if (!Int32.TryParse(member_bi.Text, out biInt))
+                    {
+                        MessageBox.Show("The BI must be an Integer!");
+                        return;
+                    }
+
+                    // DELETE THE MEMBER
+
+                    string CmdString = "football.sp_deleteMember";
+                    SqlCommand cmd_member = new SqlCommand(CmdString, con);
+                    cmd_member.CommandType = CommandType.StoredProcedure;
+                    cmd_member.Parameters.AddWithValue("@bi", biInt);
+
+                    try
+                    {
+                        con.Open();
+                        cmd_member.ExecuteNonQuery();
+                        FillDataGridMembers(con);
+                        con.Close();
+
+                        // limpar as text boxs
+                        member_name.Text = "";
+                        member_bi.Text = "";
+                        member_nif.Text = "";
+                        member_address.Text = "";
+                        member_shares_in_day.IsChecked = false;
+                        member_birth_date.Text = "";
+                        member_nationality.Text = "";
+                        member_GenderMale.IsChecked = false;
+                        member_GenderFemale.IsChecked = false;
+                        member_shares_value.Value = 0;
+                        member_number.Text = "";
+                        MessageBox.Show("The member has been deleted successfully!");
+                    }
+                    catch (Exception exc)
+                    {
+                        MessageBox.Show(exc.Message);
+                    }
+
+                }
+            }
+
+        }
     }
 }
