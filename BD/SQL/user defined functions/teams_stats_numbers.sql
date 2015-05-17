01d5b66f05add0860d0aa8b8b0a8be59871d9cf4
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-17 14:47:15 +0100

    UDF: número de jogadores por equipa e número de coachs por equipa

---
+++ BD/SQL/user defined functions/teams_stats_numbers.sql
@@ -0,0 +1,40 @@
+use p4g5;
+go
+DROP FUNCTION football.udf_number_players_per_team;
+
+go
+CREATE FUNCTION football.udf_number_players_per_team()
+RETURNS @table TABLE ("team name" varchar(50), "result" int)
+WITH SCHEMABINDING, ENCRYPTION
+AS
+BEGIN
+	-- number_players_per_team
+	INSERT @table SELECT team.name as 'team name', COUNT(play.bi) as 'result'
+					FROM football.play FULL OUTER JOIN football.team
+					ON team.name = play.team_name
+					GROUP BY team.name;
+	RETURN;
+END;
+
+go
+SELECT * FROM football.udf_number_players_per_team();
+
+go
+DROP FUNCTION football.udf_number_coachs_per_team;
+
+go
+CREATE FUNCTION football.udf_number_coachs_per_team()
+RETURNS @table TABLE ("team name" varchar(50), "result" int)
+WITH SCHEMABINDING, ENCRYPTION
+AS
+BEGIN
+	-- number_players_per_team
+	INSERT @table SELECT team.name as 'team name', COUNT(heads.bi) as 'result'
+					FROM football.heads FULL OUTER JOIN football.team
+					ON team.name = heads.team_name
+					GROUP BY team.name;
+	RETURN;
+END;
+
+go
+SELECT * FROM football.udf_number_coachs_per_team();
