17c749baeb575f0c65afbf1b06bb489d2c580f46
Rafael Ferreira <mail@rafaelferreira.pt>
2015-05-28 18:12:40 +0100

    store procedures

--- BD/SQL/store procedures/test_player_sp.sql
+++
@@ -1,7 +0,0 @@
-use p4g5;
-
-go 
-
-EXEC football.sp_createPlayer @bi = null, @name = null, @address = null, @birth_date = null,
-							  @nif = null, @gender = null, @nationality = null, @salary = null,
-							  @federation_id = null, @weight = null, @height = null;
