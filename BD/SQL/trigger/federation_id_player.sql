19e433017154dacdb95212b0616a0815157179a1
Rodrigo Cunha <rodrigocunha@ua.pt>
2015-05-31 22:07:27 +0100

    sql updates

--- BD/SQL/trigger/federation_id_player.sql
+++ BD/SQL/trigger/federation_id_player.sql
@@ -7,19 +7,19 @@ AS
 	-- check if the federation id is already in use
 	SELECT @count = count(player.federation_id) FROM football.player JOIN inserted ON player.federation_id = inserted.federation_id;
 
-	IF @count != 0
+	IF @count > 1
 	BEGIN
-		RAISERROR ('The federation id is already in use!', 14, 1)
-		RETURN
+		RAISERROR ('The federation id is already in use!', 16, 1)
+		ROLLBACK TRANSACTION
 	END
 
 	-- check if the federation id is already in use
 	SELECT @count = count(coach.federation_id) FROM football.coach JOIN inserted ON coach.federation_id = inserted.federation_id;
 
-	IF @count != 0
+	IF @count > 1
 	BEGIN
-		RAISERROR ('The federation id is already in use by one coach!', 14, 1)
-		RETURN
+		RAISERROR ('The federation id is already in use by one coach!', 16, 1)
+		ROLLBACK TRANSACTION
 	END
 
 go
@@ -33,17 +33,17 @@ AS
 	-- check if the federation id is already in use
 	SELECT @count = count(player.federation_id) FROM football.player JOIN inserted ON player.federation_id = inserted.federation_id AND player.bi != inserted.bi;
 
-	IF @count != 0
+	IF @count > 1
 	BEGIN
 		RAISERROR ('The federation id is already in use!', 14, 1)
-		RETURN
+		ROLLBACK TRANSACTION
 	END
 
 	-- check if the federation id is already in use
 	SELECT @count = count(coach.federation_id) FROM football.coach JOIN inserted ON coach.federation_id = inserted.federation_id AND coach.bi != inserted.bi;
 
-	IF @count != 0
+	IF @count > 1
 	BEGIN
 		RAISERROR ('The federation id is already in use by one coach!', 14, 1)
-		RETURN
+		ROLLBACK TRANSACTION
 	END
